9fc7f72c289ad19238b61ef3f7aa0368d24402d0
root <root@ggas.com.br>
2016-09-01 09:59:06 -0300

    Estrutura Android com Notifications

---
+++ code/GCMDemo/app/src/main/java/br/com/app/seubiu/seubiu/models/RequestBody.java
@@ -0,0 +1,21 @@
+package br.com.app.seubiu.seubiu.models;
+
+
+public class RequestBody {
+
+    private String deviceName;
+    private String deviceId;
+    private String registrationId;
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public void setRegistrationId(String registrationId) {
+        this.registrationId = registrationId;
+    }
+}
---
+++ code/GCMDemo/app/src/main/java/br/com/app/seubiu/seubiu/models/ResponseBody.java
@@ -0,0 +1,16 @@
+package br.com.app.seubiu.seubiu.models;
+
+
+public class ResponseBody {
+
+    private String result;
+    private String message;
+
+    public String getResult() {
+        return result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
