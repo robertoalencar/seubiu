fab0bb03979a58e4b791762e0c00f130b6c1f8ad
Gilberto Matos <gilbertomatos@gmail.com>
2016-10-13 15:31:54 -0300

    Add shortCode to state model

--- code/backend/models/state-model.js
+++ code/backend/models/state-model.js
@@ -2,7 +2,8 @@ module.exports = function (orm, db) {
 
    var State = db.define('State', {
         id         : { type: 'serial', key: true, mapsTo: 'id' },
-        description: { type: 'text', size: 100, unique: true, required: true, mapsTo: 'description' }
+        description: { type: 'text', size: 100, unique: true, required: true, mapsTo: 'description' },
+        shortCode  : { type: 'text', size: 2, unique: true, required: true, mapsTo: 'shortCode' }
     }, {
         collection: 'state'
     });
