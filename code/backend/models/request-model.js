daf7814321e1c239a1266dc8cd90790d48cef575
Gilberto Matos <gilbertomatos@gmail.com>
2016-11-03 13:47:18 -0300

    Add Vue.js support and a minor refactor at request model

--- code/backend/models/request-model.js
+++ code/backend/models/request-model.js
@@ -3,14 +3,17 @@ module.exports = function (orm, db) {
     var Request = db.define('Request', {
         id         : { type: 'serial', key: true, mapsTo: 'id' },
         description: { type: 'text', size: 250, mapsTo: 'description' },
-        ip         : { type: 'text', size: 255, required: true, mapsTo: 'ip' }
+        ip         : { type: 'text', size: 255, required: true, mapsTo: 'ip' },
+        address    : { type: 'text', size: 255, required: true, mapsTo: 'address' },
+        coordLat   : { type: 'number', required: true, mapsTo: 'coordLat' },
+        coordLong  : { type: 'number', required: true, mapsTo: 'coordLong' }
     }, {
         collection: 'request',
         timestamp: true
     });
 
     Request.hasOne('owner', db.models.User, { required: true, reverse: 'requests' });
-    Request.hasOne('address', db.models.UserAddress, {}, { required: true });
+    Request.hasOne('city', db.models.City, {}, { required: true });
     Request.hasOne('profession', db.models.Profession, { required: true });
     Request.hasOne('professional', db.models.User, { reverse: 'matchedRequests' });
     Request.hasOne('status', db.models.RequestStatus, { required: true });
