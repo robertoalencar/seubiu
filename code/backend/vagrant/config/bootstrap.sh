b7a37056d9558d2672c49b048430ea4bb4308e3c
Gilberto Matos <gilbertomatos@gmail.com>
2017-03-03 22:27:32 -0300

    Back Node to 4.x

--- code/backend/vagrant/config/bootstrap.sh
+++ code/backend/vagrant/config/bootstrap.sh
@@ -115,7 +115,7 @@ echo ""
 
 # Install Node.js and NPM
 apt-get remove -y nodejs
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Install Grunt
