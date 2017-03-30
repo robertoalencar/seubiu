621119dd3db82d8343e4f9a2d9c9b2feee00115f
arthur.carvalho <arthurcarvalhoc@gmail.com>
2017-03-30 13:29:03 -0300

    Ajuste de Tela de prossições - Tela 6 Protótipo

--- code/Client/app/src/main/java/br/com/orube/client/activities/JobTypeActivity.java
+++ code/Client/app/src/main/java/br/com/orube/client/activities/JobTypeActivity.java
@@ -1,20 +1,26 @@
 package br.com.orube.client.activities;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.orube.client.R;
 import br.com.orube.client.model.Profession;
 import br.com.orube.client.model.Service;
+import br.com.orube.client.util.CustomAdapter;
+import br.com.orube.client.util.Model;
 import br.com.orube.client.util.ServiceGenerator;
 import br.com.orube.client.util.SeuBiuRequest;
 import br.com.orube.client.util.SeuBiuRest;
@@ -29,6 +35,9 @@ public class JobTypeActivity extends AppCompatActivity implements View.OnClickLi
     private Spinner spinner;
     private Button button;
 
+    private Spinner spinner_servico;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +47,37 @@ public class JobTypeActivity extends AppCompatActivity implements View.OnClickLi
         spinner = (Spinner)findViewById(R.id.SpnJobType);
         button = (Button)findViewById(R.id.btnProximo);
 
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+                SeuBiuRequest.getInstance().setProfession( spinner.getSelectedItem().toString() );
+                List<Profession> professions = SeuBiuRequest.getInstance().getProfessionList();
+
+                getServices();
+
+                ListView listView = (ListView) findViewById(R.id.listView1);
+
+                List<Service> services = SeuBiuRequest.getInstance().getServiceList();
+                List<Model> modelList = new ArrayList<>();
+
+                if( services != null && !services.isEmpty() ){
+                    for(Service s : services){
+                        modelList.add(new Model(s.getDescription(), 0));
+                    }
+                    Model[] modelItems = modelList.toArray(new Model[modelList.size()]);
+
+                    CustomAdapter adapter = new CustomAdapter(context, modelItems);
+                    listView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {  }
+
+        });
+
+
         ArrayAdapter<String> combo = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1 );
 
         List<Profession> lista = SeuBiuRequest.getInstance().getProfessionList();
@@ -46,6 +86,9 @@ public class JobTypeActivity extends AppCompatActivity implements View.OnClickLi
         }
         combo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(combo);
+
+        ArrayAdapter<String> comboServico = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1 );
+        comboServico.add("");
 
         button.setOnClickListener( this );
 
@@ -57,12 +100,7 @@ public class JobTypeActivity extends AppCompatActivity implements View.OnClickLi
         Intent intent;
         if( v == button ){
             SeuBiuRequest.getInstance().setProfession( spinner.getSelectedItem().toString() );
-
-            getServices();
-
-
         }
-
     }
 
     private void avancar() {
@@ -81,7 +119,6 @@ public class JobTypeActivity extends AppCompatActivity implements View.OnClickLi
             public void onResponse(Call<List<Service>> call, Response<List<Service>> response) {
                 if(response.isSuccessful()){
                     SeuBiuRequest.getInstance().setServiceList( response.body() );
-                    avancar();
                 }else{
                     Log.d("REST", response.body() + " - " + response.message() );
                 }
