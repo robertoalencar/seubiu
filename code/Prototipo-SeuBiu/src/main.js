67ba2589fd73145857cdfba2c3bc61d51c725a0e
everton.nascimento <everton.walker@hotmail.com>
2017-04-12 15:24:19 -0300

    Implementando o css e html do login

--- code/Prototipo-SeuBiu/src/main.js
+++ code/Prototipo-SeuBiu/src/main.js
@@ -7,9 +7,6 @@ import { routes } from './routes';
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
-Vue.http.options.root = 'http://localhost:3020';
-
-
 const router = new VueRouter({ routes,
  mode: 'history' });
 
