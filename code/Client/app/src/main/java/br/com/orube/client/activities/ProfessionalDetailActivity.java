e0c46c88bc0d29f2a8ecb6ba6e7c4bbb935d37d7
arthur.carvalho <arthurcarvalhoc@gmail.com>
2017-05-05 16:16:31 -0300

    Tela de Detalhamento de Profissional

---
+++ code/Client/app/src/main/java/br/com/orube/client/activities/ProfessionalDetailActivity.java
@@ -0,0 +1,15 @@
+package br.com.orube.client.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.orube.client.R;
+
+public class ProfessionalDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professional_detail);
+    }
+}
